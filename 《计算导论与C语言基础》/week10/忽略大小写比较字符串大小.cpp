837f139cfa8198700e07d518ce1aaecf3d3bb78f
shiajih <k134563@gmail.com>
2018-03-27 22:07:04 +0800

    update

---
+++ 《计算导论与C语言基础》/week10/忽略大小写比较字符串大小.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int i = 0;
+	char result;
+	string s, v;
+	cin >> s >> v;
+	transform(s.begin(), s.end(), s.begin(), ::toupper);
+	transform(v.begin(), v.end(), v.begin(), ::toupper);
+	while (s[i] != '\0' && (s[i] == v[i])) {
+		i++;
+	}
+	if (s[i] > v[i]) {
+		result = '>';
+	}
+	else if (s[i] < v[i]) {
+		result = '<';
+	}
+	else {
+		result = '=';
+	}
+	cout << result;
+	return 0;
+}
