affa3b22dfa31051217eeb9aa4adf488d166bb7e
shiajih <k134563@gmail.com>
2018-05-02 23:54:14 +0800

    update

---
+++ 《C程序设计进阶》/week3/编程题＃3：排队游戏.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <utility>
+#include <stack>
+using namespace std;
+
+int main() {
+	stack<pair<char, int>> s;
+	char sign[100], left = ' ', right = ' ';
+	int i = 0;
+	cin.getline(sign, 100);
+	left = sign[i];
+
+	while (sign[i] == left) {
+		i++;
+	}
+	sign[i] = right;
+	i = 0;
+	while (sign[i] != '\0') {
+		if (sign[i] == left) {
+			s.push(make_pair(left, i));
+			i++;
+		}
+		else {
+			pair <char, int> p = s.top();
+			s.pop();
+			cout << p.second << ' ' << i << endl;
+			i++;
+		}
+	}
+
+}
---
+++ 《C程序设计进阶》/week3/编程题＃4：扩号匹配问题.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <utility>
+#include <stack>
+#include <cstring>
+using namespace std;
+
+int main() {
+	stack<pair<char, int>> s;
+	char sign[101] = { '\0' };
+
+	while (cin.getline(sign, 101)) {
+		int i = 0;
+		char fail[101];
+
+		for (int i = 0; i < 101; i++)
+			fail[i] = ' ';
+
+		while (sign[i] != '\0') {
+			if (sign[i] == '(') {
+				s.push(make_pair(sign[i], i));
+				i++;
+			}
+			else if (s.empty() && sign[i] == ')') {
+				fail[i] = '?';
+				i++;
+			}
+			else if (!s.empty() && sign[i] == ')') {
+				s.pop();
+				i++;
+			}
+			else {
+				i++;
+			}
+		}
+		//cout << fail;
+		cout << sign << endl;
+		while (!s.empty()) {
+			pair<char, int> p = s.top();
+			int temp = p.second;
+			fail[temp] = '$';
+			s.pop();
+		}
+
+		int good = strlen(sign);
+
+		for (int i = 0; i < good; i++) {
+			cout << fail[i];
+		}
+		cout << endl;
+	}
+	return 0;
+}
